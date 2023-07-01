5bfbc967ee2f14ee5c995cd25eb271315ae3153d
ditrin <Shlacoblock@327>
2023-07-01 10:43:31 +0300

    fix adapter

--- app/src/main/java/com/example/todolist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/todolist/presentation/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.example.todolist.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.todolist.R
+import com.yandex.authsdk.YandexAuthLoginOptions
+import com.yandex.authsdk.YandexAuthOptions
+import com.yandex.authsdk.YandexAuthSdk
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +23,33 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             setCurrentFragment(MainScreenFragment())
         }
+
+        val sdk = YandexAuthSdk(
+            this, YandexAuthOptions(this)
+        )
+        val loginOptionsBuilder = YandexAuthLoginOptions.Builder()
+        val intent = sdk.createLoginIntent(loginOptionsBuilder.build())
+
+        val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK){
+                val yandexAuthToken = sdk.extractToken(it.resultCode, it.data)
+                if (yandexAuthToken != null){
+
+
+                    Log.d("Testus", "$yandexAuthToken.value")
+                    supportFragmentManager.beginTransaction()
+                        .add(R.id.fragmentContainer, MainScreenFragment())
+                        .commit()
+                }
+
+            } else{
+                Log.d("Test", "error")
+            }
+
+        }
+        launcher.launch(intent)
     }
+
 
     private fun setCurrentFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply {
